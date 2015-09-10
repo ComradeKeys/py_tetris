135ae78d6fd6e0de02d2e44e97b91b38a5535624
ComradeKeys <key13005@byui.edu>
2015-09-10 13:37:46 -0600

    Set up the initialization

--- main.py
+++ main.py
@@ -2,33 +2,40 @@ import pygame
 from pygame.locals import *
 
 def main():
-    # Initialise screen
-    pygame.init()
-    screen = pygame.display.set_mode((400, 600))
-    pygame.display.set_caption('py_Tetris')
+
+  #Initialize pyGame
+  pygame.init()
+  pygame.font.init()
+  screen  = pygame.display.set_mode((400, 600))
+  clock   = pygame.time.Clock()
+  done    = False
+  pygame.display.set_caption('py_tetris')
+  font = pygame.font.SysFont("Liberation Sans", 72)
+
+  # Fill background
+  background = pygame.Surface(screen.get_size())
+  background = background.convert()
+  background.fill((15, 192, 252))
     
-    # Fill background
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    background.fill((15, 192, 252))
-    
-    # Display some text
-    font = pygame.font.Font(None, 36)
-    text = font.render("py_Tetris", 1, (10, 10, 10))
-    textpos = text.get_rect()
-    textpos.centerx = background.get_rect().centerx
-    background.blit(text, textpos)
-    
-    # Blit everything to the screen
-    screen.blit(background, (0, 0))
+  # Display some text
+  font = font.render("py_tetris", 1, (10, 10, 10))
+  textpos = font.get_rect()
+  textpos.centerx = background.get_rect().centerx
+
+  while not done:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        done = True
+      if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+        done = True
+      background.blit(font, textpos)
+      screen.blit(background, (0, 0))
+ppppp
     pygame.display.flip()
-    
-    # Event loop
-    while 1:
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                return
-        screen.blit(background, (0, 0))
-        pygame.display.flip()
-        
+    clock.tick(60)
+
 if __name__ == '__main__': main()
+
+#being nice to the operating system
+pygame.font.quit()
+pygame.quit()
